be180f155ff41782ff656bda381a2b2200ec0107
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-24 14:53:30 +0000

    Released version 1.3.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/svconnector_feed/trunk@70295 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "svconnector_feed".
-#
-# Auto generated 23-09-2012 18:41
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "svconnector_feed".
+ *
+ * Auto generated 24-01-2013 15:53
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Connector service - FEED',
 	'description' => 'Connector service for XML files or RSS feeds',
 	'category' => 'services',
 	'shy' => 0,
-	'version' => '1.2.0',
+	'version' => '1.3.0',
 	'dependencies' => 'svconnector',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"d45f";s:16:"ext_autoload.php";s:4:"c230";s:21:"ext_conf_template.txt";s:4:"ef02";s:12:"ext_icon.gif";s:4:"d043";s:17:"ext_localconf.php";s:4:"b595";s:10:"README.txt";s:4:"4f8f";s:42:"Resources/Public/Samples/Configuration.txt";s:4:"d799";s:14:"doc/manual.pdf";s:4:"011d";s:14:"doc/manual.sxw";s:4:"6a50";s:36:"sv1/class.tx_svconnectorfeed_sv1.php";s:4:"02f3";s:17:"sv1/locallang.xml";s:4:"a236";}',
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"fad3";s:16:"ext_autoload.php";s:4:"c230";s:21:"ext_conf_template.txt";s:4:"ef02";s:12:"ext_icon.gif";s:4:"d043";s:17:"ext_localconf.php";s:4:"b595";s:10:"README.txt";s:4:"4f8f";s:42:"Resources/Public/Samples/Configuration.txt";s:4:"d799";s:14:"doc/manual.pdf";s:4:"011d";s:14:"doc/manual.sxw";s:4:"6a50";s:36:"sv1/class.tx_svconnectorfeed_sv1.php";s:4:"02f3";s:17:"sv1/locallang.xml";s:4:"a236";}',
 	'suggests' => array(
 	),
 );
