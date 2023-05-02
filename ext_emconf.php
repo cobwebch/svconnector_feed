22938fa96521239fd726a97cd1a17d8a8a0a0308
Francois Suter <>
2023-05-02 22:23:38 +0200

    [TASK] Verify compatibility with TYPO3 12 LTS, relase version 4.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Connector service - XML Feed',
     'description' => 'Connector service for XML files or RSS feeds',
     'category' => 'services',
-    'version' => '4.0.0',
+    'version' => '4.0.1',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '11.5.0-12.1.99',
+                    'typo3' => '11.5.0-12.4.99',
                     'svconnector' => '5.0.0-0.0.0',
                 ],
             'conflicts' =>
