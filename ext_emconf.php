fd865f7639794edf0f79982d8f4a5ecd63312f89
Francois Suter <sutfra@gmail.com>
2024-11-26 18:57:40 +0100

    [TASK] Verify compatibility with TYPO3 13 ELTS, resolves #11

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '12.4.0-13.1.99',
+                    'typo3' => '12.4.0-13.4.99',
                     'svconnector' => '5.0.0-0.0.0',
                 ],
             'conflicts' =>
