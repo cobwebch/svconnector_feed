23316d71571c3a833895a9f65615c0d89756254b
Francois Suter <sutfra@gmail.com>
2024-12-08 18:16:58 +0100

    [TASK] Release version 5.0.0

--- Documentation/Index.rst
+++ Documentation/Index.rst
@@ -27,7 +27,7 @@ Feed/XML Connector Service
 		  xml, rss, feed, data import, fetch data
 
 	:Copyright:
-		2009-2024
+		2009-2025
 
 	:Author:
 		  François Suter (Idéative)
--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Connector service - XML Feed',
     'description' => 'Connector service for XML files or RSS feeds',
     'category' => 'services',
-    'version' => '4.1.0',
+    'version' => '5.0.0',
     'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'Francois Suter (Idéative)',
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
             'depends' =>
                 [
                     'typo3' => '12.4.0-13.4.99',
-                    'svconnector' => '5.0.0-0.0.0',
+                    'svconnector' => '6.0.0-0.0.0',
                 ],
             'conflicts' =>
                 [
